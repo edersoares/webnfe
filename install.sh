ba020510c81a136ec90d8e81043fde3dc8f24ef6
Eder Soares <edersoares@me.com>
2017-11-22 20:01:41 -0200

    Updated docker-compose.yml, install.sh and created .env.api file

--- install.sh
+++ install.sh
@@ -1,27 +1,30 @@
+#!/usr/bin/env bash
+
 # API Install
 
 echo "[WebNF-e] API install.."
 echo "[API] Composer install.."
-docker run --rm -v $(pwd)/application/api:/application/api -w /application/api composer install
+#docker run --rm -v $(pwd)/application/api:/application/api -w /application/api composer install
 
 docker run --rm \
     -v $(pwd)/application/api:/application/api \
     -w /application/api \
+    --env-file $(pwd)/.env.api \
     php bash -c "
         echo '[API] Creating database..'
         touch database/database.sqlite
         echo '[API] Permissions..'
         chmod -R 777 storage
         chmod -R 777 bootstrap/cache
-        echo '[API] Key..'
-        php artisan key:generate
         echo '[API] Migration..'
-        php artisan migrate
+        php artisan migrate --force
     "
+
+# Client Install
 
 echo "[WebNF-e] Client install.."
 echo "[Client] NPM install.."
-docker run --rm \
-    -v $(pwd)/application/client:/application/client \
-    -w /application/client \
-    node npm install
+#docker run --rm \
+#    -v $(pwd)/application/client:/application/client \
+#    -w /application/client \
+#    node npm install
